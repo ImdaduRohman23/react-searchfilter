16755f38296bb8f58a7897b9647bd8b9e9b903b4
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-10-09 10:22:22 +0700

    react basic search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import './App.css';
+import { Users } from './Users';
 
 function App() {
+  const [search, setSearch] = useState('');
+
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <input type="text" className="search" placeholder='Search . . .' onChange={(e) => setSearch(e.target.value)} />
+      <ul className="list">
+        {
+          Users
+            .filter(i=> i.first_name.toLowerCase().includes(search.toLowerCase()))
+            .map(i => (
+              <li key={i.id} className="list__item">{i.first_name}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
---
+++ src/Users.js
@@ -0,0 +1,20 @@
+export const Users = [
+    {
+        'id': 0,
+        'first_name':'Imdadu',
+        'last_name': 'Rohman',
+        'email': 'imdadu@mail.com'
+    },
+    {
+        'id': 1,
+        'first_name':'Alikul',
+        'last_name': 'Majda',
+        'email': 'alikul@mail.com'
+    },
+    {
+        'id': 2,
+        'first_name':'Ahmad',
+        'last_name': 'Isna Mustafid',
+        'email': 'imdadu@mail.com'
+    }
+]
